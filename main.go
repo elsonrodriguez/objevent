9668e56d27262e8246f5b281ebea0b727253968f
Elson Rodriguez <elson.rodriguez@gmail.com>
2018-06-15 20:37:58 -0700

    AWS flow is working

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/sns"
@@ -14,76 +15,81 @@ func main() {
 	svc := sns.New(sess)
 	s3svc := s3.New(sess)
 
-        bucketname := "ultrapuke"
-	endpointurl := "http://google.com"
+        bucketName := os.Getenv("OBJEVENT_BUCKET_NAME")
+	endpointURL := os.Getenv("OBJEVENT_ENDPOINT_URL")
 
-	topicparams := &sns.CreateTopicInput{
-		Name: aws.String(bucketname),
+	topicParams := &sns.CreateTopicInput{
+		Name: aws.String(bucketName),
 	}
 
-        topicresp, err := svc.CreateTopic(topicparams)
+        topicResponse, err := svc.CreateTopic(topicParams)
 
 	if err != nil { 
 		fmt.Println(err.Error())
 		return
 	}
 
-	fmt.Println(topicresp)
+	fmt.Println(topicResponse)
 
-        subparams := &sns.SubscribeInput{
-		Endpoint: aws.String(endpointurl),
+        subParams := &sns.SubscribeInput{
+		Endpoint: aws.String(endpointURL),
 		Protocol: aws.String("http"), // Parse this out later		
-		TopicArn: topicresp.TopicArn,
+		TopicArn: topicResponse.TopicArn,
 	}
 
-	subresp, err := svc.Subscribe(subparams)
+	subscriptionResponse, err := svc.Subscribe(subParams)
 
 	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
 
-	fmt.Println(subresp)
+	fmt.Println(subscriptionResponse)
 
-	snspermparams := &sns.AddPermissionInput{
+	snsPermParams := &sns.AddPermissionInput{
 		AWSAccountId: aws.StringSlice([]string{"*",}),
-		ActionName: aws.StringSlice([]string{"SNS:Subscribe","SNS:Receive"}),
+		ActionName: aws.StringSlice([]string{"Subscribe","Receive","Publish"}),
 		Label: aws.String("objevent default policy"),
-		TopicArn: topicresp.TopicArn,
+		TopicArn: topicResponse.TopicArn,
 	}
 
-	snspermresp, err := svc.AddPermission(snspermparams)
+	snsPermResponse, err := svc.AddPermission(snsPermParams)
 
-	fmt.Println(snspermresp)	
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	fmt.Println("Permissions be like:")
+	fmt.Println(snsPermResponse)
+
+	//need to confirm subscription with token... might need some app logic, the app needs to have a token endpoint, maybe use the ARN as a unique path. endpointURL/tokens/{arn}/
 
 
-	//now need to confirm subscription with token... might need some app logic, the app needs to have a token endpoint, maybe use the ARN as a unique path. /tokens/{arn}/
-
-        eventlist := "s3:ObjectCreated:*"
-
-	topicconfigparams := []*s3.TopicConfiguration{
+	topicConfigParams := []*s3.TopicConfiguration{
 		&s3.TopicConfiguration {
-			Events: []*string{aws.String(eventlist)},
-			TopicArn: topicresp.TopicArn,
+			Events: []*string{aws.String("s3:ObjectCreated:*")},
+			TopicArn: topicResponse.TopicArn,
 		},
 	}
 
-	notificationparams := &s3.NotificationConfiguration{
-		TopicConfigurations: topicconfigparams,
+	notificationParams := &s3.NotificationConfiguration{
+		TopicConfigurations: topicConfigParams,
 	}
 
-	bucketnotificationparams := &s3.PutBucketNotificationConfigurationInput{
-		Bucket: aws.String(bucketname),
-		NotificationConfiguration: notificationparams,
+	bucketnotificationParams := &s3.PutBucketNotificationConfigurationInput{
+		Bucket: aws.String(bucketName),
+		NotificationConfiguration: notificationParams,
 	}
 
-	notificationresp, err := s3svc.PutBucketNotificationConfiguration(bucketnotificationparams)
+	notificationResponse, err := s3svc.PutBucketNotificationConfiguration(bucketnotificationParams)
 
 	if err != nil {
  		fmt.Println(err.Error())
 		return
         }     
 
-	fmt.Println(notificationresp)
+	fmt.Println(notificationResponse)
 
+	fmt.Printf("Added event notifications for bucket %s to endpoint %s", bucketName, endpointURL)
 }
